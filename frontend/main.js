23f082ffa4f882c1a59dad5ccabebcc9682384ce
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-09 00:21:31 +0530

    improve files

---
+++ frontend/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: true,
+        speed: 1500, 
+        sync: true,
+        in: {
+            effect:
+        }
+    })
+};
