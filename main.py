b70f74cf2dc0bb04672706a8aa30cefb4dadabac
Amit Singh <abhaybhadouriya88@gmail.com>
2026-01-09 21:52:01 +0530

    update files

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+import eel
+eel.init("frontend")
+os.system('start msedge.exe --app="http://192.168.1.2:5500/frontend/index.html"')
+
+eel.start("index.html", mode=None, host="localhost",block=True)
